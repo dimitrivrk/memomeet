babf7573c8df4cbbe23ac2da741785cfc73bc8d5
dimitrivrk <dimitri.varakine@outlook.fr>
2025-04-07 03:23:50 -0400

    fix : authconfig

--- src/app/api/auth/auth.config.ts
+++ src/app/api/auth/auth.config.ts
@@ -1,7 +1,18 @@
+import GoogleProvider from "next-auth/providers/google";
 import type { NextAuthOptions } from "next-auth";
 
 export const authConfig: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
   pages: {
     signIn: '/login',
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: 'jwt',
   },
 };
