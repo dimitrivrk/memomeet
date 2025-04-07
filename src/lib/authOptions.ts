a648616ee627efad2700b86f91aef2c8dc525ff4
dimitrivrk <dimitri.varakine@outlook.fr>
2025-04-07 14:52:24 -0400

    unused value

--- src/lib/authOptions.ts
+++ src/lib/authOptions.ts
@@ -36,7 +36,7 @@ export const authOptions: NextAuthOptions = {
       }
       return session;
     },
-    async redirect({ url, baseUrl }) {
+    async redirect({baseUrl }) {
       return baseUrl; // 👈 redirection vers la home après connexion
     },
   },
