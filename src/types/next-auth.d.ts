ddaef47f97ef97ae06a1eba9fc51fdd827c5b311
dimitrivrk <dimitri.varakine@outlook.fr>
2025-04-07 00:48:18 -0400

    next auth import not used

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,15 +1,15 @@
-import NextAuth from "next-auth";
-
+// PAS BESOIN D'IMPORTER "next-auth" MANUELLEMENT
 declare module "next-auth" {
-  interface Session {
-    user: {
-      id: string;
-      email: string;
-      name?: string | null;
-      image?: string | null;
-      credits?: number;
-      subscription?: 'none' | 'standard' | 'pro';
-      isUnlimited?: boolean;
-    };
+    interface Session {
+      user: {
+        id: string;
+        email: string;
+        name?: string | null;
+        image?: string | null;
+        credits?: number;
+        subscription?: 'none' | 'standard' | 'pro';
+        isUnlimited?: boolean;
+      };
+    }
   }
-}
+  
