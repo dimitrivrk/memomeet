92de73bb520acc77de0231b303dfa7b467272c6b
dimitrivrk <dimitri.varakine@outlook.fr>
2025-04-07 02:47:09 -0400

    maj nextauth d ts

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,14 +1,30 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import NextAuth from "next-auth";
+// NO IMPORTS HERE !
+// Juste le module declare
 
-declare module "next-auth" {
+declare module 'next-auth' {
   interface Session {
     user: {
+      id: string; // 👈 C'est ça qui manquait
       name?: string | null;
       email?: string | null;
       image?: string | null;
       subscription?: string;
       credits?: number;
+      isUnlimited?: boolean;
     };
+  }
+
+  interface User {
+    id: string; // 👈 Ça aussi il vaut mieux l'ajouter ici
+    subscription?: string;
+    credits?: number;
+    isUnlimited?: boolean;
+  }
+
+  interface JWT {
+    id?: string;
+    subscription?: string;
+    credits?: number;
+    isUnlimited?: boolean;
   }
 }
